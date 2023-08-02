7cb1dae1d955e314b61b7da58336568e1d08a9ee
Phyllali <90408077+phyllali@users.noreply.github.com>
2023-08-03 00:08:37 +0800

    update color picker

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const PIXEL_SIZE = 32;
   let isMouseDown = false;
   let isEraserMode = false;
+  let isPickColor = false;
   let currentColor = "#000000";
   let colorHistory = [];
   let pixelsData = [];
@@ -60,14 +61,26 @@ document.addEventListener("DOMContentLoaded", function () {
   clearButton.addEventListener("click", clearCanvas);
 
   // Get the value of the color picker and update the current selected pen color and color history
-  const colorPicker = document.getElementById("color-picker");
-  colorPicker.addEventListener("input", function () {
-    const newColor = colorPicker.value;
+  // const colorPicker = document.getElementById("color-picker");
+  // colorPicker.addEventListener("input", function () {
+  //   const newColor = colorPicker.value;
+  //   if (newColor !== currentColor) {
+  //     currentColor = newColor;
+  //     updateColorHistory(newColor);
+  //   }
+  // });
+
+  const colorDisplay = document.getElementById("colorDisplay");
+  const setColor = document.getElementById("setColor");
+  // Function to set color
+  function setUpColor() {
+    const newColor = colorDisplay.style.backgroundColor;
     if (newColor !== currentColor) {
       currentColor = newColor;
       updateColorHistory(newColor);
     }
-  });
+  }
+  setColor.addEventListener("click", setUpColor);
 
   // Update the color history
   function updateColorHistory(color) {
@@ -166,11 +179,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // When clicking on a pixel on the canvas, get its color and update the color picker value
   function pickColorFromPixel(event) {
-    const pixel = event.target;
-    const pickedColor = getColorFromPixel(pixel);
-    if (pickedColor !== "transparent") {
-      currentColor = pickedColor;
-      colorPicker.value = pickedColor;
+    if (isPickColor) {
+      const pixel = event.target;
+      const pickedColor = getColorFromPixel(pixel);
+      if (pickedColor !== "transparent") {
+        currentColor = pickedColor;
+        colorPicker.value = pickedColor;
+      }
+    } else {
+      const pixel = event.target;
+      if (isEraserMode) {
+        pixel.style.backgroundColor = "transparent";
+        pixel.dataset.color = "transparent";
+      } else {
+        pixel.style.backgroundColor = currentColor;
+        pixel.dataset.color = currentColor; // Update the color attribute of the pixel
+      }
     }
   }
 
@@ -252,3 +276,220 @@ document.getElementById("uploadInput").addEventListener("change", function (even
   };
   reader.readAsDataURL(file);
 });
+
+const colorDisplay = document.getElementById("colorDisplay");
+const redRange = document.getElementById("redRange");
+const greenRange = document.getElementById("greenRange");
+const blueRange = document.getElementById("blueRange");
+const hueRange = document.getElementById("hueRange");
+const saturationRange = document.getElementById("saturationRange");
+const valueRange = document.getElementById("valueRange");
+const hexInput = document.getElementById("hexInput");
+const darkenBtn = document.getElementById("darkenBtn");
+const lightenBtn = document.getElementById("lightenBtn");
+
+let updatingColor = false; // Flag to prevent infinite loop during color updates
+
+// Function to update color display
+function updateColorDisplay() {
+  const red = redRange.value;
+  const green = greenRange.value;
+  const blue = blueRange.value;
+  const hue = hueRange.value;
+  const saturation = saturationRange.value;
+  const value = valueRange.value;
+  const hex = rgbToHex(red, green, blue);
+
+  colorDisplay.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  hexInput.value = hex;
+}
+
+// Function to convert RGB to HEX
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+// Function to update RGB sliders and color display based on HEX input
+function updateColorFromHex() {
+  const hex = hexInput.value;
+  const rgb = hexToRgb(hex);
+
+  if (rgb) {
+    redRange.value = rgb.r;
+    greenRange.value = rgb.g;
+    blueRange.value = rgb.b;
+    updateColorDisplay();
+  }
+}
+
+// Function to convert HEX to RGB
+function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+    }
+    : null;
+}
+
+// Function to update color display based on HSV sliders
+function updateColorFromHsv() {
+  if (!updatingColor) {
+    updatingColor = true;
+    const hue = hueRange.value;
+    const saturation = saturationRange.value / 100;
+    const value = valueRange.value / 100;
+    const rgb = hsvToRgb(hue, saturation, value);
+    redRange.value = rgb.r;
+    greenRange.value = rgb.g;
+    blueRange.value = rgb.b;
+    updateColorDisplay();
+    updatingColor = false;
+  }
+}
+
+// Function to update color display based on RGB sliders
+function updateColorFromRgb() {
+  if (!updatingColor) {
+    updatingColor = true;
+    const red = redRange.value;
+    const green = greenRange.value;
+    const blue = blueRange.value;
+    const hsv = rgbToHsv(red, green, blue);
+    hueRange.value = hsv.h;
+    saturationRange.value = hsv.s * 100;
+    valueRange.value = hsv.v * 100;
+    updateColorDisplay();
+    updatingColor = false;
+  }
+}
+
+// Function to convert HSV to RGB
+function hsvToRgb(h, s, v) {
+  const c = v * s;
+  const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+  const m = v - c;
+  let r, g, b;
+  if (h >= 0 && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (h >= 60 && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (h >= 120 && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (h >= 180 && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (h >= 240 && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  return {
+    r: Math.round((r + m) * 255),
+    g: Math.round((g + m) * 255),
+    b: Math.round((b + m) * 255),
+  };
+}
+
+// Function to convert RGB to HSV
+function rgbToHsv(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  const d = max - min;
+  let h, s, v;
+
+  if (d === 0) {
+    h = 0;
+  } else if (max === r) {
+    h = ((g - b) / d) % 6;
+  } else if (max === g) {
+    h = (b - r) / d + 2;
+  } else {
+    h = (r - g) / d + 4;
+  }
+
+  h = Math.round(h * 60);
+  if (h < 0) {
+    h += 360;
+  }
+
+  s = max === 0 ? 0 : d / max;
+  v = max;
+
+  return { h, s, v };
+}
+
+// Event listeners for RGB sliders and HEX input
+redRange.addEventListener("input", updateColorFromRgb);
+greenRange.addEventListener("input", updateColorFromRgb);
+blueRange.addEventListener("input", updateColorFromRgb);
+hexInput.addEventListener("input", updateColorFromHex);
+
+// Event listeners for HSV sliders
+hueRange.addEventListener("input", updateColorFromHsv);
+saturationRange.addEventListener("input", updateColorFromHsv);
+valueRange.addEventListener("input", updateColorFromHsv);
+
+// Function to darken color
+function darkenColor() {
+  const currentValue = parseInt(valueRange.value);
+  if (currentValue > 0) {
+    valueRange.value = currentValue - 5;
+    updateColorFromHsv(); // Update color based on HSV sliders after adjusting value
+  }
+}
+
+// Function to lighten color
+function lightenColor() {
+  const currentValue = parseInt(valueRange.value);
+  if (currentValue < 100) {
+    valueRange.value = currentValue + 5;
+    updateColorFromHsv(); // Update color based on HSV sliders after adjusting value
+  }
+}
+
+// Event listeners for darken and lighten buttons
+darkenBtn.addEventListener("click", darkenColor);
+lightenBtn.addEventListener("click", lightenColor);
+
+// Function to handle preset color button click
+function handlePresetColorButtonClick(event) {
+  const color = event.target.getAttribute("data-color");
+  const rgb = hexToRgb(color);
+  const hsv = rgbToHsv(rgb.r, rgb.g, rgb.b);
+
+  redRange.value = rgb.r;
+  greenRange.value = rgb.g;
+  blueRange.value = rgb.b;
+  hueRange.value = hsv.h;
+  saturationRange.value = hsv.s * 100;
+  valueRange.value = hsv.v * 100;
+
+  updateColorDisplay();
+}
+
+// Event listeners for preset color buttons
+const presetColorButtons = document.querySelectorAll(".presetColor");
+presetColorButtons.forEach((button) => {
+  button.addEventListener("click", handlePresetColorButtonClick);
+});
+
+// Initialize color display
+updateColorDisplay();
